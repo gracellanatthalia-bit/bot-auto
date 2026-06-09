35b313bd2f3a888298f9be7de521907920c53149
gracellanatthalia-bit <gracellanatthalia@gmail.com>
2026-06-09 17:09:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-require("dotenv").config();
+if (!process.env.BOT_TOKEN) {
+  throw new Error("BOT_TOKEN belum terbaca di Railway");
+}
 
-const express = require("express");
-const { Telegraf } = require("telegraf");
-
-const app = express();
-const bot = new Telegraf(process.env.BOT_TOKEN);
-
-app.use(express.json());
-
-bot.start((ctx) => {
-  ctx.reply("Selamat datang di Bot Auto Order!");
-});
-
-app.get("/", (req, res) => {
-  res.send("Bot berjalan dengan baik");
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-bot.launch();
+const bot = new Telegraf(process.env.BOT_TOKEN.trim());
